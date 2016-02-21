fdc14356d226ad703490ee2d1b505e27ea8b2c3c
Mahfuzur Rahman <mahfuz@student.sust.edu>
2016-02-21 10:02:37 +0600

    index up

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ def index():
 #    return "Hello from Python!"
 
 # launch
-#if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
